7a9b95a1f1cc6b04693a3c0f9484d1d4e0baaf7c
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-09-22 21:41:04 +0600

    Blog Post view

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/admin', 'AdminsController@index')->name('admin.index');
+Route::get('/post', 'PostController@show')->name('post');
+
