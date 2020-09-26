8e8ad000012bac396ddb5f36a5895e4c5914f750
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-09-27 01:30:48 +0600

    Created a post from admin - update1

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,15 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
-Route::get('/admin', 'AdminsController@index')->name('admin.index');
 Route::get('/post/{post}', 'PostController@show')->name('post');
+
+
+Route::middleware('auth')->group(function(){
+
+Route::get('/admin', 'AdminsController@index')->name('admin.index');
+Route::get('/admin/posts/create', 'PostController@create')->name('post.create');
+Route::post('/admin/posts', 'PostController@store')->name('post.store');
+
+
+});
 
