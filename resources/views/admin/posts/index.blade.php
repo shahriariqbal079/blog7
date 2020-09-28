137a4064a357d8903db62fa021970a897c35800a
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-09-28 22:37:38 +0600

    Displaying the owner of the post

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -12,6 +12,7 @@
                         <thead>
                             <tr>
                                 <th>Id</th>
+                                <th>Owner</th>
                                 <th>Title</th>
                                 <th>Image</th>
                                 <th>Created</th>
@@ -33,6 +34,7 @@
                             @foreach ($posts as $post)
                             <tr>
                             <td>{{$post->id}}</td>
+                            <td>{{$post->user->name}}</td>
                             <td>{{$post->title}}</td>
                             <td>
                                 <img height="40px" src="{{$post->post_image}}" alt="">
